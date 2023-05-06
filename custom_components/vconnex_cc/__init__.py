2c36916210548362a83f62d9118a6dad9e1c90f6
Hieu LE <hieulq19@gmail.com>
2023-05-06 16:30:06 +0700

    Fix for hass version 2023.5.x

--- custom_components/vconnex_cc/__init__.py
+++ custom_components/vconnex_cc/__init__.py
@@ -22,7 +22,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         return False
 
     hass.data[DOMAIN][entry.entry_id] = vconnex_data
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
